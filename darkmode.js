4d7529e15c07d0c118d7e9eecec7146239feeaef
shreykoradia <67724256+shreykoradia@users.noreply.github.com>
2020-11-29 17:51:52 +0530

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,4 @@
-alert('HOLA fxer i wasted my 2 hrs for this insane stuff or shit i dont know ');
+
  const themeSwitch = document.querySelector('input');
 
 themeSwitch.addEventListener('change', () => {  
