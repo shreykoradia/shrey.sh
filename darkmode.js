f73c62ec2a50a48d327decb0f39a81499bc0e6e5
shreykoradia <67724256+shreykoradia@users.noreply.github.com>
2020-11-29 09:59:22 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+alert('HOLA fxer i wasted my 2 hrs for this insane stuff or shit i dont know ');
+ const themeSwitch = document.querySelector('input');
+
+themeSwitch.addEventListener('change', () => {  
+  document.body.classList.toggle('dark-mode');
+});
